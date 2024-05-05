7ec0fd554567ce018024e3eb91a701555fd81ffe
MrMati <mati.niedbala5@gmail.com>
2024-05-05 11:44:43 +0200

    main cleanup

--- device/main.py
+++ device/main.py
@@ -1,13 +1,2 @@
-import machine
-
-# fully turns off common anode LED
-
-pins_high = [2, 3, 4]
-for pin_num in pins_high:
-    pin = machine.Pin(pin_num, machine.Pin.OUT)
-    pin.high()
-
-
-# start BLu app
 import app_main
 app_main.go()
